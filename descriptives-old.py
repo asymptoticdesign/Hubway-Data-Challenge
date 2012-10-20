c5fab926c0242956e8f2653982b412a77b01144b
nathan lachenmyer <asymptoticdesign@mit.edu>
2012-10-20 00:29:22 -0400

    Commit on faraday-cage pre-merge

--- descriptives-old.py
+++ descriptives-old.py
@@ -21,20 +21,42 @@ def flattenToList(input):
 	output = list(reduce(lambda p,q: p + q, input))
 	return output
 
-def dateQuery(dateField,subField="month",filename="output"):
+def agg_stats(fieldName):
+	minQuery = "SELECT MIN(" + fieldName + ") FROM trips"
+	min = hubway.MySQLquery(minQuery)[0][0]
+	print "Field Mininum:",min
+	maxQuery = "SELECT MAX(" + fieldName + ") FROM trips"
+	max = hubway.MySQLquery(maxQuery)[0][0]
+	print "Field Maximum:",max
+	avgQuery = "SELECT AVG(" + fieldName + ") FROM trips"
+	avg = hubway.MySQLquery(avgQuery)[0][0]
+	print "Field Mean:",avg
+	stdQuery = "SELECT STDDEV_SAMP(" + fieldName + ") FROM trips"
+	std = hubway.MySQLquery(stdQuery)[0][0]
+	print "Field Std:",std
+	return [min,max,avg,std]
+
+def dateQuery(dateField,subField="month"):
 	queryString = "SELECT %s(%s), COUNT(*) AS frequency FROM trips GROUP BY %s(%s) ORDER BY %s;" %(subField.upper(), dateField, subField.upper(), dateField, dateField)
 	freq = hubway.MySQLquery(queryString)
-	#write to file here
 	return freq[1:]
 
-def modal_stats(fieldName,filename="output"):
+def modal_stats(fieldName):
 	freqQuery = "SELECT %s, COUNT(%s) AS frequency FROM trips GROUP BY %s ORDER BY %s;" %(fieldName,fieldName,fieldName,fieldName)
 	freqs = hubway.MySQLquery(freqQuery)
 #	print "Mode:",freqs[0]
 	print "Unique Items:",len(freqs)
-	#write to file here
 	return freqs[1:]
 	
+def median(fieldName):
+	count = hubway.MySQLquery("SELECT COUNT(" + fieldName + ") FROM trips;")[0][0]
+	if count %2 == 0:
+		medQuery = "SELECT " + fieldName + " FROM trips ORDER BY " + fieldName + " LIMIT " + str(count/2) + ",1;"
+		median = hubway.MySQLquery(medQuery)
+	else:
+		medQuery = "SELECT " + fieldName + " FROM trips ORDER BY " + fieldName + " LIMIT " + str(int(scipy.floor(count/2))) + ",2;"
+		median = hubway.MySQLquery(medQuery)
+		return median
 
 def plotData(frequencies,cutoff=-1,abscissa='Absicca [units]',ordinate='No. [counts]',title='Hubway DataViz'):
 	bins = []
@@ -47,3 +69,8 @@ def plotData(frequencies,cutoff=-1,abscissa='Absicca [units]',ordinate='No. [cou
 	pylab.ylabel(ordinate,{'fontsize':20})
 	pylab.title(title,{'fontsize':20})
 	pylab.show()
+
+#ideas:
+#track individual bikes through city
+#distance to T stations
+#distance to nearest item
